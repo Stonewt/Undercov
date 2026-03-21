648412cfeb1810c59428373481fdc8029e15ab62
Ramzy Bekhoukh <rbekhoukh@gmail.com>
2026-03-21 19:49:50 +0100

    Monetag sur le site

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10760017
+    "zoneId": 10760027
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
